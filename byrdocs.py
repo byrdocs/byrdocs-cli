305f5c680d7823a0b893b516ceba90bf80189ea0
NoNormalCreeper <2560359315@qq.com>
2024-11-16 15:17:55 +0800

    优化参数提示

--- byrdocs.py
+++ byrdocs.py
@@ -58,71 +58,72 @@ if not token_path.exists():
 with token_path.open("r") as f:
     token = f.read().strip()
 
-if not args.file:
-    print("Error: No file specified for upload.")
-    exit(1)
+if args.command == 'upload' or args.file:
+    if not args.file:
+        print("Error: No file specified for upload.")
+        exit(1)
 
-file = args.file
+    file = args.file
 
-with open(file, "rb") as f:
-    md5 = hashlib.md5(f.read()).hexdigest()
+    with open(file, "rb") as f:
+        md5 = hashlib.md5(f.read()).hexdigest()
 
-print(f"Uploading {md5}.pdf ...")
+    print(f"Uploading {md5}.pdf ...")
 
-payload = json.dumps(
-    {
-        "key": md5 + ".pdf",    # TODO: 多类型文件上传支持
-    }
-)
-headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
-
-response = requests.request(
-    "POST", f"{baseURL}/api/s3/upload", headers=headers, data=payload
-)
-
-data = response.json()
-
-if not data["success"]:
-    print(response.text)    # TODO: 优化失败处理
-    exit(1)
-
-print(f"{md5}.pdf status: `Pending`")
-input("Press Enter to continue uploading...")
-
-temporary_credentials = {
-    "AccessKeyId": data["credentials"]["access_key_id"],
-    "SecretAccessKey": data["credentials"]["secret_access_key"],
-    "SessionToken": data["credentials"]["session_token"],
-}
-
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=temporary_credentials["AccessKeyId"],
-    aws_secret_access_key=temporary_credentials["SecretAccessKey"],
-    aws_session_token=temporary_credentials["SessionToken"],
-    region_name="us-east-1",
-    endpoint_url="https://s3.byrdocs.org",
-)
-
-bucket_name = "test"
-file_name = file
-object_name = data["key"]
-
-try:
-    s3_client.upload_file(
-        file_name,
-        bucket_name,
-        object_name,
-        ExtraArgs={
-            "Tagging": "&".join(
-                [f"{key}={value}" for key, value in data["tags"].items()]
-            )
-        },
+    payload = json.dumps(
+        {
+            "key": md5 + ".pdf",    # TODO: 多类型文件上传支持
+        }
     )
-    print("File uploaded successfully")
-    print(f"\tFile URL: {baseURL}/files/{md5}.pdf")
-    print(f"{md5}.pdf status: `Uploaded`")
-except (NoCredentialsError, PartialCredentialsError) as e:
-    print(f"Credential error: {e}")
-except Exception as e:
-    print(f"Error uploading file: {e}")
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+
+    response = requests.request(
+        "POST", f"{baseURL}/api/s3/upload", headers=headers, data=payload
+    )
+
+    data = response.json()
+
+    if not data["success"]:
+        print(response.text)    # TODO: 优化失败处理
+        exit(1)
+
+    print(f"{md5}.pdf status: `Pending`")
+    input("Press Enter to continue uploading...")
+
+    temporary_credentials = {
+        "AccessKeyId": data["credentials"]["access_key_id"],
+        "SecretAccessKey": data["credentials"]["secret_access_key"],
+        "SessionToken": data["credentials"]["session_token"],
+    }
+
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=temporary_credentials["AccessKeyId"],
+        aws_secret_access_key=temporary_credentials["SecretAccessKey"],
+        aws_session_token=temporary_credentials["SessionToken"],
+        region_name="us-east-1",
+        endpoint_url="https://s3.byrdocs.org",
+    )
+
+    bucket_name = "test"
+    file_name = file
+    object_name = data["key"]
+
+    try:
+        s3_client.upload_file(
+            file_name,
+            bucket_name,
+            object_name,
+            ExtraArgs={
+                "Tagging": "&".join(
+                    [f"{key}={value}" for key, value in data["tags"].items()]
+                )
+            },
+        )
+        print("File uploaded successfully")
+        print(f"\tFile URL: {baseURL}/files/{md5}.pdf")
+        print(f"{md5}.pdf status: `Uploaded`")
+    except (NoCredentialsError, PartialCredentialsError) as e:
+        print(f"Credential error: {e}")
+    except Exception as e:
+        print(f"Error uploading file: {e}")
