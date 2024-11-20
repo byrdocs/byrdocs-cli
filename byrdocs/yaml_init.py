5f8745feacf15b82487b4c03e65d854d89f0b1ad
NoNormalCreeper <2560359315@qq.com>
2024-11-21 04:46:04 +0800

    WIP: ask file name

--- byrdocs/yaml_init.py
+++ byrdocs/yaml_init.py
@@ -20,17 +20,17 @@ def format_filename(file_name: str) -> str | None:
     for pre in prefixs:
         file_name = file_name.removeprefix(pre)
     if file_name.endswith('.pdf'):
-        surfix = '.pdf'
+        suffix = '.pdf'
     elif file_name.endswith('.zip'):
-        surfix = '.zip'
+        suffix = '.zip'
     else:
         return None
-    file_name = file_name.removesuffix(surfix)
+    file_name = file_name.removesuffix(suffix)
     if len(file_name) == 32:
-        return file_name + surfix
+        return file_name + suffix
     return None
 
-def ask_for_init(file_name: str=None) -> str:
+def ask_for_init(file_name: str=None) -> str:   # 若需要传入 file_name，需要带上后缀名
     questions: list[dict[str, str]] = []
     if file_name is None:
         file_name = inquirer.text(
@@ -39,7 +39,10 @@ def ask_for_init(file_name: str=None) -> str:
             invalid_message="Invaild file name."
         ).execute()
     file_name = format_filename(file_name)
-    # print(file_name)
+    metadata["id"] = file_name[:-4]
+    metadata["url"] = f"https://byrdocs.org/files/{file_name}"
+    
+
 
 
 ask_for_init()
