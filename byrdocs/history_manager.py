e89f57c4c2d47930f331d9ea857d6071cea30cc7
NoNormalCreeper <2560359315@qq.com>
2024-12-02 11:24:42 +0800

    WIP: use JSON to store history

--- byrdocs/history_manager.py
+++ byrdocs/history_manager.py
@@ -1,42 +1,66 @@
+import json
 from pathlib import Path
 
-history_path = Path.home() / ".config" / "byrdocs" / "history"
+history_path = Path.home() / ".config" / "byrdocs" / "history.json"
+
+'''
+File format:
+{
+    "history": [
+        {
+            "file": "file_path",
+            "md5": "md5",
+            "timestamp": "timestamp"
+        }
+    ]
+}
+'''
 
 class UploadHistory:
     def __init__(self):
-        self.history: list[list[str]] = []  # [file, md5, timestamp]
-        if history_path.exists():
-            self._read()
-        else:
-            history_path.touch()
+        self.data: dict[str, list[dict[str, str]]] = {}
+        self.history: list[dict[str, str]] = []
+        try:
+            if history_path.exists():
+                self._read()
+            else:
+                history_path.touch()
+                self.history = []
+                self.data = {"history": self.history}
+        except json.JSONDecodeError:
             self.history = []
+            self.data = {"history": self.history}
+            self._write()
     
     def _read(self) -> None:
         with history_path.open("r") as f:
-            try:
-                self.history = [line.split(":") for line in f.read().splitlines()]
-            except:
-                raise ValueError("无法读取历史记录文件，可能已经损坏")
+            self.data = json.load(f)
+            self.history = self.data.get("history", [])
     
     def _write(self):
+        self.data["history"] = self.history
         with history_path.open("w") as f:
-            for line in self.history:
-                f.write(":".join(line) + "\n")
+            json.dump(self.data, f, indent=4)
     
     def _with_update(func):
         def wrapper(self, *args, **kwargs):
             self._read()
             result = func(self, *args, **kwargs)
+            self.data["history"] = self.history
             self._write()
             return result
         return wrapper
 
     @_with_update
-    def add(self, file, md5, timestamp):
-        self.history.append([file, md5, str(timestamp)])
+    def add(self, file: str, md5: str, timestamp: str):
+        self.history.append({
+            "file": file,
+            "md5": md5,
+            "timestamp": timestamp
+        })
         
     @_with_update  
-    def get(self) -> list[list[str]]:
+    def get(self) -> list[dict[str, str]]:
         return self.history
     
     @_with_update
@@ -48,37 +72,70 @@ class UploadHistory:
         self.history.pop(index)
         
 class Tests:
-    def __init__(self):
-        self.history = UploadHistory()
-        self.history.clear()
-    
     def test_add(self):
-        self.history.add("test1", "md5", "time")
-        print(self.history.get())
-        assert self.history.get() == [["test1", "md5", "time"]]
-        
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        assert history.get() == [{"file": "file", "md5": "md5", "timestamp": "timestamp"}]
+    
     def test_remove(self):
-        self.history.add("test2", "md5", "time")
-        self.history.remove(0)
-        # print(self.history.get())
-        assert self.history.get() == [["test2", "md5", "time"]]
-        
-    # def test_clear(self):
-    #     self.history.add("test1", "md5", "time")
-    #     self.history.add("test2", "md5", "time")
-    #     self.history.clear()
-    #     assert self.history.get() == []
-        
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.remove(0)
+        assert history.get() == []
+    
+    def test_clear(self):
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.clear()
+        assert history.get() == []
+    
     def test_get(self):
-        self.history.add("test1", "md5", "time")
-        self.history.add("test2", "md5", "time")
-        assert self.history.get() == [["test1", "md5", "time"], ["test2", "md5", "time"]]
-        
-    def run(self):
-        self.test_add()
-        self.test_remove()
-        self.test_clear()
-        self.test_get()
-        print("All tests passed.")
-        
-# Tests().run()
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        assert history.get() == [{"file": "file", "md5": "md5", "timestamp": "timestamp"}]
+    
+    def test_multiple_add(self):
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.add("file2", "md52", "timestamp2")
+        assert history.get() == [
+            {"file": "file", "md5": "md5", "timestamp": "timestamp"},
+            {"file": "file2", "md5": "md52", "timestamp": "timestamp2"}
+        ]
+    
+    def test_multiple_remove(self):
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.add("file2", "md52", "timestamp2")
+        history.remove(0)
+        assert history.get() == [
+            {"file": "file2", "md5": "md52", "timestamp": "timestamp2"}
+        ]
+    
+    def test_multiple_clear(self):
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.add("file2", "md52", "timestamp2")
+        history.clear()
+        assert history.get() == []
+    
+    def test_multiple_get(self):
+        history = UploadHistory()
+        history.add("file", "md5", "timestamp")
+        history.add("file2", "md52", "timestamp2")
+        assert history.get() == [
+            {"file": "file", "md5": "md5", "timestamp": "timestamp"},
+            {"file": "file2", "md5": "md52", "timestamp": "timestamp2"}
+        ]
+    
+
+if __name__ == "__main__":
+    tests = Tests()
+    tests.test_clear()
+    # tests.test_add()
+    # tests.test_remove()
+    # tests.test_multiple_add()
+    # tests.test_multiple_remove()
+    # tests.test_multiple_get()
+    # tests.test_multiple_clear()
+    # tests.test_multiple_add()
